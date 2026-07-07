304cdc28ea3638e0eb6d9605bedda5366686524e
Paulo <paulostaciano@gmail.com>
2026-07-07 00:06:13 -0300

    inicial commit

---
+++ src/test/java/com/organizaai/OrganizaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organizaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
